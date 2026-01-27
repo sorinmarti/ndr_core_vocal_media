266a021b367112c08c1d7dd5ae5361c433d26052
sorin <sorin.marti@gmail.com>
2026-01-27 14:56:21 +0100

    Added project files

---
+++ ndr/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from ndr_core.views import dispatch, display_schema_or_404
+from ndr_core.ndr_settings import NdrSettings
+
+app_name = NdrSettings.APP_NAME
+
+urlpatterns = [
+    path('', dispatch, name='index'),
+    path('schemas/<str:schema_name>', display_schema_or_404, name='display_schema'),
+    path('p/<str:ndr_page>/', dispatch, name='ndr_view'),
+
+    path('robots.txt', dispatch, name='robots'),
+    path('sitemap.xml', dispatch, name='sitemap'),
+
+    path(r"[\w\d]*\.html", dispatch, name="html_page"),
+]
